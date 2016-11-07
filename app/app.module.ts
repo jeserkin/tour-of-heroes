68ec18f95201b9d99240bfec26cfcd930f6fd47c
jeserkin <jeserkin@gmail.com>
2016-11-07 09:07:43 +0200

    initial commit

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeroesComponent } from "./heroes.component";
+import { HeroDetailComponent } from "./hero-detail.component";
+import { HeroService } from "./hero.service";
+import { DashboardComponent } from "./dashboard.component";
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent
+  ],
+  providers: [HeroService],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
